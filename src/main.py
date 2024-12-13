dd2c9edbf3f5254ce7a62bf71d3e6404348977ab
omarlari <omarlari@gmail.com>
2024-12-13 12:34:20 -0800

    added cors and json capability

--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
-import flask
+import os, logging
+from flask import Flask, jsonify
+from flask_cors import CORS
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+app.logger.setLevel(logging.DEBUG)
+CORS(app)
 
 @app.route('/')
 def index():
     return "Hello World!"
+
+@app.route('/test')
+def appRoot():
+    person = {'name': 'PR request 1', 'birth-year': 1979}
+    return jsonify(person)
 
 if __name__ == '__main__':
     app.run()
